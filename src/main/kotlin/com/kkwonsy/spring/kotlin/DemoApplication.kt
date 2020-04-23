39a9127b4a1a30af38573697a897a463bce54964
sungyoungKwon85 <ssyang0@naver.com>
2020-04-23 16:25:18 +0900

    겹치는 타입의 Bean일 때 어느걸 쓸지 정해야 한다

--- src/main/kotlin/com/kkwonsy/spring/kotlin/DemoApplication.kt
+++ src/main/kotlin/com/kkwonsy/spring/kotlin/DemoApplication.kt
@@ -2,6 +2,7 @@ package com.kkwonsy.spring.kotlin
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
@@ -11,16 +12,22 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
+    @Bean
+    @ConditionalOnExpression("#{'\${service.message.type}' == 'simple'}")
+    fun exampleService(): ServiceInterface = ExampleService()
 
-@Bean
-fun exampleService(): ServiceInterface = ExampleService()
+    @Bean
+    @ConditionalOnExpression("#{'\${service.message.type}' == 'advance'}")
+    fun advancedService(): ServiceInterface = AdvanceService()
+}
+
 
 @Controller
-class FirstController {
+class FirstController() {
 
-//    @Autowired
-    lateinit var exampleService: ExampleService
+    @Autowired
+    lateinit var exampleService: ServiceInterface
     // lateinit을 선언하면 이 프로퍼티는 생성자 다음에 초기화 된다
 
     @RequestMapping(value = ["/user/{name}"], method = arrayOf(RequestMethod.GET))
