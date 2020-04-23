869aa59bc651d7a46381fe4e075c7294d2780022
sungyoungKwon85 <ssyang0@naver.com>
2020-04-23 13:44:06 +0900

    hello world

--- src/main/kotlin/com/kkwonsy/spring/kotlin/DemoApplication.kt
+++ src/main/kotlin/com/kkwonsy/spring/kotlin/DemoApplication.kt
@@ -2,9 +2,19 @@ package com.kkwonsy.spring.kotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class DemoApplication
+
+@Controller
+class FirstController {
+    @RequestMapping
+    @ResponseBody
+    fun hello() = "hello world"
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
