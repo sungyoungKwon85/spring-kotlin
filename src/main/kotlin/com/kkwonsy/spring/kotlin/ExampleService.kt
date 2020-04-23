d5c817419c32b2896242aad73cdbd0caa2987200
sungyoungKwon85 <ssyang0@naver.com>
2020-04-23 16:05:33 +0900

    SpEL

--- src/main/kotlin/com/kkwonsy/spring/kotlin/ExampleService.kt
+++ src/main/kotlin/com/kkwonsy/spring/kotlin/ExampleService.kt
@@ -1,9 +1,19 @@
 package com.kkwonsy.spring.kotlin
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class ExampleService : ServiceInterface{
 
-    override fun getHello(name: String) = "hello $name"
+    @Value(value = "\${service.message.text:ola}")
+    private lateinit var text: String
+
+    @Value(value = "#{4+3}")
+    private lateinit var result1: Number
+
+    @Value(value = "#{ \${one.value} div \${another.value} }")
+    private lateinit var result2: Number
+
+    override fun getHello(name: String) = "$text $name $result1 $result2"
 }
